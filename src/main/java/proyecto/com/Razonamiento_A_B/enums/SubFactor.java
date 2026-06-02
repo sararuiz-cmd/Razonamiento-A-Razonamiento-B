4e9abb9af52efb17f31e0cdc988b891a8c62219a
Argonauta0 <moisesvalle@uamv.edu.ni>
2026-06-02 14:41:40 -0600

    feat(model): agregando clase enum SubFactor

---
+++ src/main/java/proyecto/com/Razonamiento_A_B/enums/SubFactor.java
@@ -0,0 +1,6 @@
+package proyecto.com.Razonamiento_A_B.enums;
+
+public enum SubFactor {
+    R1, // Representa implícitamente a la Forma A (Ítems 1-22)
+    R2  // Representa implícitamente a la Forma B (Ítems 23-55)
+}

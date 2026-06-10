4b86e5adec2107e6c8d80936ab7db9eb6fb1c700
Argonauta0 <moisesvalle@uamv.edu.ni>
2026-06-09 20:49:47 -0600

    fix: actualizando las clases SubFactor y Tipoltem

--- src/main/java/proyecto/com/Razonamiento_A_B/enums/SubFactor.java
+++ src/main/java/proyecto/com/Razonamiento_A_B/enums/SubFactor.java
@@ -1,6 +1,17 @@
 package proyecto.com.Razonamiento_A_B.enums;
 
 public enum SubFactor {
-    R1, // Representa implícitamente a la Forma A (Ítems 1-22)
-    R2  // Representa implícitamente a la Forma B (Ítems 23-55)
+    R1("R1 - Forma A"),
+    R2("R2 - Forma B");
+
+    private final String descripcion;
+
+    SubFactor(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return descripcion;
+    }
 }
--- src/main/java/proyecto/com/Razonamiento_A_B/enums/TipoItem.java
+++ src/main/java/proyecto/com/Razonamiento_A_B/enums/TipoItem.java
@@ -1,6 +1,17 @@
 package proyecto.com.Razonamiento_A_B.enums;
 
 public enum TipoItem {
-    NUMERICO,
-    VERBAL
+    NUMERICO("Numérico"),
+    VERBAL("Verbal");
+
+    private final String descripcion;
+
+    TipoItem(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return descripcion;
+    }
 }

f0be3320e47aca736a629009809d1ebefbc6d1bd
MAbrahamVA <moisesvalle@uamv.edu.ni>
2026-06-15 15:55:46 -0600

    fix: enum TipoItem

--- src/main/java/proyecto/com/Razonamiento_A_B/enums/TipoItem.java
+++ src/main/java/proyecto/com/Razonamiento_A_B/enums/TipoItem.java
@@ -1,17 +1,9 @@
 package proyecto.com.Razonamiento_A_B.enums;
 
 public enum TipoItem {
-    NUMERICO("Numérico"),
-    VERBAL("Verbal");
-
-    private final String descripcion;
-
-    TipoItem(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    @Override
-    public String toString() {
-        return descripcion;
-    }
+    SERIE_NUMERICA,
+    SERIE_ALFABETICA,
+    ANALOGIA_VERBAL,
+    RAZONAMIENTO_LOGICO,
+    COMPLETACION
 }

114c1db7a897c8eaa52553dac30866e40ad5a8f9
MAbrahamVA <moisesvalle@uamv.edu.ni>
2026-06-15 15:55:17 -0600

    fix: enum SubFactor

--- src/main/java/proyecto/com/Razonamiento_A_B/enums/SubFactor.java
+++ src/main/java/proyecto/com/Razonamiento_A_B/enums/SubFactor.java
@@ -1,17 +1,7 @@
 package proyecto.com.Razonamiento_A_B.enums;
 
 public enum SubFactor {
-    R1("R1 - Forma A"),
-    R2("R2 - Forma B");
-
-    private final String descripcion;
-
-    SubFactor(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    @Override
-    public String toString() {
-        return descripcion;
-    }
+    R1,
+    R2,
+    RT
 }
